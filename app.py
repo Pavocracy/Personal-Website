1a0032018cc415f398c2ac226a34e49b78d9eba9
pavocracy <pavocracy@pm.me>
2022-03-12 19:43:43 +1000

    update route

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, redirect
+from os import system
+from time import sleep
+from hmac import new, compare_digest
+from app_secrets import update_hash
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    if app.env == "development":
+        return
+    if request.is_secure:
+        return
+    url = request.url.replace("http://", "https://", 1)
+    code = 301
+    return redirect(url, code=code)
 
 
 @app.route("/")
@@ -13,5 +28,24 @@ def giftideas():
     return render_template("gift_ideas.html")
 
 
+@app.route("/update", methods=["POST"])
+def update():
+    sig_header = "X-Hub-Signature-256"
+    if sig_header in request.headers:
+        secret_hash = request.headers[sig_header].split("=")
+        if len(secret_hash) == 2:
+            req_sign = secret_hash[1]
+            computed_sign = new(update_hash, request.data, hashlib.sha256).hexdigest()
+            if compare_digest(req_sign, computed_sign):
+                threading.Thread(
+                    target=lambda: [
+                        sleep(2),
+                        system("systemctl restart personalwebsite.service"),
+                    ]
+                ).start()
+                return "update call successful"
+    return "update call failed"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
