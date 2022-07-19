c8bbc78c839cec0d34a464a671dab65a4b7c975b
Pavocracy <pavocracy@pm.me>
2022-07-19 11:33:43 +1000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-date > updated.txt
+date >> updated.txt
 
 git fetch
 git reset origin/master --hard
 git pull
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
